13cb06fe1fa3514fab6589c98f1b4a620f1b9218
luis <lualreye@gmail.com>
2022-02-07 10:26:12 -0500

    feat: server on with fake package

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const faker = require('faker')
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hoa mi server en exoress');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hoa soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  const products = []
+  for(let i = 0; i < 100; i++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    })
+  }
+  res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 2000
+  })
+})
+
+app.get('/users', (req, res) => {
+  const{limit, offset} = req.query
+  if(limit && offset) {
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.send('No hay parametros')
+  }
+})
+
+app.get('/categories/:categoryId/productos/:productoId', (req,res) => {
+  const { categoryId, productoId } = req.params
+  res.json({
+    categoryId,
+    productoId
+  })
+})
+
+app.listen(port, () => {
+  console.log('Mi port' + port);
+});
