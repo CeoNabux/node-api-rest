4acf9f875007bcdb8f7a11fcdaee0c95fe190e28
luis <lualreye@gmail.com>
2022-02-07 23:20:24 -0500

    feat: middleware schema validator handlera added in product router

--- middlewares/validator.handler.js
+++ middlewares/validator.handler.js
@@ -3,9 +3,9 @@ const boom = require('@hapi/boom');
 function validatorHandler(schema, property) {
   return (req, res, next) => {
     const data = req[property];
-    const { error } = schema.validate(data);
+    const { error } = schema.validate(data, {abortEarly: false});
     if (error) {
-      next(boom.badRequest());
+      next(boom.badRequest(error));
     }
     next();
   };
--- routes/products.router.js
+++ routes/products.router.js
@@ -43,22 +43,29 @@ router.get('/:id',
 //   res.send('yo soy un filtro')
 // })
 
-router.post('/', async (req, res, next) => {
-  const body = req.body;
-  const newProduct = await service.create(body);
-  res.status(201).json(newProduct);
-});
-
-router.patch('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
+router.post('/',
+  validatorHandler(createProductSchema, 'body'),
+async (req, res, next) => {
     const body = req.body;
-    const product = await service.update(id, body);
-    res.json(product);
-  } catch (error) {
-    next(error);
+    const newProduct = await service.create(body);
+    res.status(201).json(newProduct);
   }
-});
+);
+
+router.patch('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const product = await service.update(id, body);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
